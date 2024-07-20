7f44a2e7d4f0111e88cd0869aad05f453eb43172
Rian <142219160+Rianofc@users.noreply.github.com>
2024-07-20 12:42:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1657,7 +1657,27 @@ async function jadianimennya(url) {
   return animenya
 }
 // ini buktinya
-
+function lirik(judul){
+  return new Promise(async(resolve, reject) => {
+      axios.get('https://www.musixmatch.com/search/' + judul)
+      .then(async({ data }) => {
+      const $ = cheerio.load(data)
+      const hasil = {};
+      let limk = 'https://www.musixmatch.com'
+      const link = limk + $('div.media-card-body > div > h2').find('a').attr('href')
+        await axios.get(link)
+        .then(({ data }) => {
+          const $$ = cheerio.load(data)
+          hasil.thumb = 'https:' + $$('div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div').find('img').attr('src')
+          $$('div.col-sm-10.col-md-8.col-ml-6.col-lg-6 > div.mxm-lyrics').each(function(a,b) {
+       hasil.lirik = $$(b).find('span > p > span').text() +'\n' + $$(b).find('span > div > p > span').text()
+       })
+     })
+     resolve(hasil)
+   })
+   .catch(reject)
+   })
+}
 async function getPinterestImages(text) {
   const url = 'https://www.pinterest.com/resource/BaseSearchResource/get/';
   const params = {
@@ -2959,6 +2979,43 @@ app.get('/api/gemini', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+app.get('/api/lirik', async (req, res) => {
+  try {
+    const message = req.query.judul;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
+    }
+    let down = await lirik(message) 
+    res.status(200).json({
+      status: 200,
+      creator: "RIAN X EXONITY",
+      result: down
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post('/ai/logic/post', async (req, res) => {
+	try {
+		const logic = req.logic;
+                const message = req.message;
+		if (!logic) {
+			return res.status(400).send('No file uploaded.');
+		}
+               if (!message) {
+			return res.status(400).send('No file uploaded.');
+		}
+		let down = await gptlogicnya(logic, message) 
+    res.status(200).json({
+      status: 200,
+      creator: "RIAN X EXONITY",
+      result: down
+    });
+	} catch (error) {
+		console.error(error);
+		res.json(mess.error)
+	}
 });
 app.get('/api/gptlogic', async (req, res) => {
   try {
