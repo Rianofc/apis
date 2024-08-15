2867a43cdca30a6f6d7877dc71f5ee39ae7807f0
Rian <142219160+Rianofc@users.noreply.github.com>
2024-08-15 10:17:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const os = require('os');
 const {
   v4: uuidv4
 } = require("uuid")
+const Canvas = require('canvas')
 const canvafy = require('canvafy');
 const Canvacord = require("canvacord");
 const got = require('got')
+const canvasGif = require('canvas-gif')
 const bodyParser = require('body-parser');
 const { load } = require('cheerio');
 const { stringify } = require('qs');
@@ -3499,6 +3501,61 @@ app.get('/api/sfile', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+app.get('/api/attp', async (req, res) => {
+	var text = req.query.text
+	if (!text ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter text"})
+
+const file = "./files/attp.gif"
+
+let length = text.length
+
+var font =90
+
+if (length>12){ font = 68}
+if (length>15){ font = 58}
+if (length>18){ font = 55}
+if (length>19){ font = 50}
+if (length>22){ font = 48}
+if (length>24){ font = 38}
+if (length>27){ font = 35}
+if (length>30){ font = 30}
+if (length>35){ font = 26}
+if (length>39){ font = 25}
+if (length>40){ font = 20}
+if (length>49){ font = 10}
+Canvas.registerFont('./files/SF-Pro.ttf', { family: 'SF-Pro' })
+await canvasGif(
+	file,(ctx) => {
+var couler = ["#ff0000","#ffe100","#33ff00","#00ffcc","#0033ff","#9500ff","#ff00ff"]
+let jadi = couler[Math.floor(Math.random() * couler.length)]
+
+		function drawStroked(text, x, y) {
+			ctx.lineWidth = 5
+			ctx.font = `${font}px SF-Pro`
+			ctx.fillStyle = jadi
+			ctx.strokeStyle = 'black'
+			ctx.textAlign = 'center'
+			ctx.strokeText(text, x, y)
+			ctx.fillText(text, x, y)
+		}
+		
+		drawStroked(text,290,300)
+
+	},
+	{
+		coalesce: false, // whether the gif should be coalesced first (requires graphicsmagick), default: false
+		delay: 0, // the delay between each frame in ms, default: 0
+		repeat: 0, // how many times the GIF should repeat, default: 0 (runs forever)
+		algorithm: 'octree', // the algorithm the encoder should use, default: 'neuquant',
+		optimiser: false, // whether the encoder should use the in-built optimiser, default: false,
+		fps: 7, // the amount of frames to render per second, default: 60
+		quality: 100, // the quality of the gif, a value between 1 and 100, default: 100
+	}
+).then((buffer) =>{
+res.set({'Content-Type': 'gif'})
+res.send(buffer)
+
+})
 app.get('/api/sfile-search', async (req, res) => {
   try {
     const message = req.query.query;
